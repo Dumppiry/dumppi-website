e89688af0e2f36bae2a2e366550637527f323b7e
rennehir <git@rennehir.fi>
2020-03-13 22:16:45 +0200

    Render big people card

--- web/src/sections/big-people-section.js
+++ web/src/sections/big-people-section.js
@@ -1,13 +1,20 @@
 import React from "react"
 import styled from "styled-components"
 
-import Link from "../components/link"
 import PortableText from "../components/portable-text/heading"
+import Person from "../components/big-person-card"
 
 const BigPeopleSection = ({ heading, people }) => {
   return (
     <S.Section>
       <PortableText blocks={heading} />
+      {people.length > 0 && (
+        <S.People>
+          {people.map(person => (
+            <S.Person person={person} />
+          ))}
+        </S.People>
+      )}
     </S.Section>
   )
 }
@@ -29,4 +36,15 @@ S.Section = styled.section`
     font-size: 1.25rem;
     color: #949494;
   }
+`
+
+S.People = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
+
+S.Person = styled(Person)`
+  width: calc(100% / 3 - 1.5rem);
+  margin: 1.5rem 0;
 `
