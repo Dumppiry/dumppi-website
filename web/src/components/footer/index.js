d8c4304c4b7fe71dbd4be46e8cd69b6d67007c08
rennehir <git@rennehir.fi>
2020-03-20 13:40:04 +0200

    Add some footer styles steelos we bring many kilos

--- web/src/components/footer/index.js
+++ web/src/components/footer/index.js
@@ -1,8 +1,23 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import styled from "styled-components"
 
 const Footer = () => {
-  return <S.Footer>Moro</S.Footer>
+  const { settings } = useStaticQuery(graphql`
+    query FooterQuery {
+      settings: sanitySettings {
+        siteTitle
+      }
+    }
+  `)
+
+  return (
+    <S.Footer>
+      <S.Content>
+        <h2>{settings.siteTitle}</h2>
+      </S.Content>
+    </S.Footer>
+  )
 }
 
 export default Footer
@@ -12,4 +27,17 @@ const S = {}
 S.Footer = styled.footer`
   width: 100%;
   margin-top: 60px;
+  padding: 5rem 1rem;
+  background-color: #292929;
+  color: white;
+`
+
+S.Content = styled.div`
+  max-width: 940px;
+  margin: auto;
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 500;
+  }
 `
