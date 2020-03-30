1777a8696b0203c053a26c666ccca38198ce8ea7
papivaan <vaananen.paavo@gmail.com>
2020-03-30 22:40:44 +0300

    remove antd, use react-table instead

--- studio/components/EventRegistrationSubmissionsPreview.js
+++ studio/components/EventRegistrationSubmissionsPreview.js
@@ -1,6 +1,65 @@
 import React, { useEffect, useState } from "react";
 import client from "part:@sanity/base/client";
-import { Table } from "antd";
+import { useTable } from "react-table";
+
+// This the simplest example found on the interwebs
+const Table = ({ data, columns }) => {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  });
+
+  // Render the UI for your table
+  return (
+    <table style={{ borderCollapse: "collapse" }} {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th
+                style={{ border: "1px solid black", padding: "1rem" }}
+                {...column.getHeaderProps()}
+              >
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr style={{ height: "10rem" }} {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (
+                  <td
+                    style={{
+                      border: "1px solid black",
+                      padding: "1rem",
+                      verticalAlign: "top",
+                      maxHeight: "20rem"
+                    }}
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 const EventRegistrationSubmissionsPreview = ({ document }) => {
   const { registrationForm, registrationSubmissions } = document.displayed;
@@ -32,8 +91,18 @@ const EventRegistrationSubmissionsPreview = ({ document }) => {
       setSubmissions(subs);
     });
   }, [registrationSubmissions]);
-
-  return <Table dataSource={submissions} columns={columns} />;
+  const headers = columns.map(c => ({
+    Header: c.title,
+    accessor: c.dataIndex
+  }));
+  return submissions &&
+    submissions.length > 0 &&
+    columns &&
+    columns.length > 0 ? (
+    <Table data={submissions} columns={headers} />
+  ) : (
+    <p>wait</p>
+  );
 };
 
 export default EventRegistrationSubmissionsPreview;
