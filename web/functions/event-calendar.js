8e8d2e099664865b9769baec635a3ca8685c26a2
rennehir <git@rennehir.fi>
2020-04-23 22:05:58 +0300

    Add actual events to calendar

--- web/functions/event-calendar.js
+++ web/functions/event-calendar.js
@@ -1,4 +1,16 @@
 const ics = require("ics")
+const sanityClient = require("@sanity/client")
+const moment = require("moment")
+
+const { sanity } = require("../client-config")
+const { SANITY_READ_TOKEN } = process.env
+
+const client = sanityClient({
+  projectId: sanity.projectId,
+  dataset: sanity.dataset,
+  token: SANITY_READ_TOKEN,
+  useCdn: true,
+})
 
 const event = {
   start: [2020, 4, 25, 6, 30],
@@ -11,10 +23,55 @@ const event = {
 }
 
 exports.handler = async (evt, context) => {
-  const { error, value } = ics.createEvent(event)
+  const { lang = "fi" } = evt.queryStringParameters
 
-  return {
-    statusCode: 200,
-    body: value,
+  try {
+    const query = `
+      *[_type == "event"]{
+        ...,
+        location->{...}
+      }
+    `
+
+    const results = await client.fetch(query)
+
+    const events = results.map((event) => {
+      const url = `https://confident-heisenberg-a5b89e.netlify.app/${
+        lang === "fi" ? "tapahtumat" : "en/events"
+      }/${event.slug[lang ? lang : "fi"].current}/`
+
+      const start = moment(event.startDate)
+      const end = moment(event.endDate)
+
+      const duration = moment.duration(end.diff(start))
+
+      return {
+        start: [
+          start.year(),
+          start.month(),
+          start.day(),
+          start.hours(),
+          start.minutes(),
+        ],
+        duration: { hours: duration.hours(), minutes: duration.minutes() },
+        title: event.title[lang ? lang : "fi"],
+        description: `${url}`,
+        location: `${event.location.title}, ${event.location.address}`,
+        url,
+      }
+    })
+
+    const { error, value: calendar } = ics.createEvents(events)
+
+    return {
+      statusCode: 200,
+      body: calendar,
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }, null, 2),
+    }
   }
 }
