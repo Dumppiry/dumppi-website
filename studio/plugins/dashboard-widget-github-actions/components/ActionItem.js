3ae1d9182df9d6ede7eec4640704b6028f488fb0
rennehir <renne@rennehir.fi>
2020-07-22 23:22:20 +0300

    Show snackbar notifications

--- studio/plugins/dashboard-widget-github-actions/components/ActionItem.js
+++ studio/plugins/dashboard-widget-github-actions/components/ActionItem.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import Button from "part:@sanity/components/buttons/default";
+import Snackbar from "part:@sanity/components/snackbar/default";
 
 import styles from "./ActionItem.css";
 
@@ -10,6 +11,7 @@ const ActionItem = ({ action, repo }) => {
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
 
   const triggerAction = () => {
     setLoading(true);
@@ -20,29 +22,56 @@ const ActionItem = ({ action, repo }) => {
       }),
     })
       .then((response) => {
-        console.log("Done");
         setLoading(false);
+
+        response.statusCode === 204
+          ? setSuccess(true)
+          : setError(response.statusMessage);
       })
       .catch((error) => {
         setError(error);
         setLoading(false);
       });
   };
+
+  const renderErrorSnackbar = () => (
+    <Snackbar
+      kind="error"
+      title="An error occured"
+      subtitle={error}
+      children={<>Please try again.</>}
+      onClose={() => setError(null)}
+    />
+  );
+
+  const renderSuccessSnackbar = () => (
+    <Snackbar
+      kind="success"
+      title="Action triggered"
+      children={<>Click the name of the action to see its progress</>}
+      onClose={() => setSuccess(false)}
+    />
+  );
+
   return (
-    <li className={styles.item}>
-      <div>
-        <a
-          href={`https://github.com/${repo.owner}/${repo.name}/actions?query=workflow%3A"${name}"`}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img src={badge_url} alt={name} />
-        </a>
-      </div>
-      <Button inverted loading={loading} onClick={triggerAction}>
-        Deploy
-      </Button>
-    </li>
+    <>
+      {error && renderErrorSnackbar()}
+      {success && renderSuccessSnackbar()}
+      <li className={styles.item}>
+        <div>
+          <a
+            href={`https://github.com/${repo.owner}/${repo.name}/actions?query=workflow%3A"${name}"`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img src={badge_url} alt={name} />
+          </a>
+        </div>
+        <Button inverted loading={loading} onClick={triggerAction}>
+          Deploy
+        </Button>
+      </li>
+    </>
   );
 };
 
