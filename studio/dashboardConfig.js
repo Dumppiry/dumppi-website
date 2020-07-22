209c3fc212073f581df6ef52c9af336068119e9a
rennehir <renne@rennehir.fi>
2020-07-22 16:12:06 +0300

    Remove Netlify widget

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -11,19 +11,5 @@ export default {
         github_token: process.env.SANITY_STUDIO_GH_TOKEN,
       },
     },
-    {
-      name: "netlify",
-      options: {
-        title: "Netlify deploys",
-        sites: [
-          {
-            title: "dumppi.fi",
-            apiId: "7e2f1f4a-d981-44b3-a4e2-09ed0f606b3e",
-            buildHookId: "5e8b837280d09101aecf89dd",
-            name: "confident-heisenberg-a5b89e",
-          },
-        ],
-      },
-    },
   ],
 };
