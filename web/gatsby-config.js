8f8b4085f06235fed534a2cf8dff2300d34350dd
rennehir <renne@rennehir.fi>
2020-08-04 22:22:27 +0300

    Add Google Analytics

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -48,5 +48,18 @@ module.exports = {
         id: "fullpage-menu",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-174510815-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   ],
 }
