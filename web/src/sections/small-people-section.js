4d09defe1d0e7c868566429f0931d0d5e3199370
Ben-PP <karel.parkkola@gmail.com>
2022-06-11 10:54:45 +0300

    Simpler way :)

--- web/src/sections/small-people-section.js
+++ web/src/sections/small-people-section.js
@@ -13,9 +13,9 @@ const SmallPeopleSection = ({ heading, groups }) => {
           <S.Group key={section._key}>
             <PortableText blocks={section.heading} />
             <S.People>
-              {(section.people != undefined) ? section.people.map((person) => (
+              {section.people?.map((person) => (
                 <Person key={person._id} person={person} />
-              )) : null}
+              ))}
             </S.People>
           </S.Group>
         ))}
