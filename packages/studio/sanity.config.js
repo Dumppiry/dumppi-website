7061d770276961fe3bc91fc45cb20d1f10f62273
Karel Parkkola <karel.parkkola@gmail.com>
2023-11-17 23:22:28 +0200

    Added GithubActionsWidget

--- packages/studio/sanity.config.js
+++ packages/studio/sanity.config.js
@@ -7,23 +7,39 @@ import { dashboardTool, projectInfoWidget } from "@sanity/dashboard";
 import { catsWidget } from "sanity-plugin-dashboard-widget-cats";
 import deskStructure from "./deskStructure";
 import { DownloadAttendeesAction } from "./actions/DownloadAttendeesAction";
+import { githubActionsWidget } from "./plugins/sanity-github-actions-widget";
 
 export default defineConfig({
   title: "dumppi-website",
   projectId: "ubo8m1s0",
   dataset: "production",
   plugins: [
+    dashboardTool({
+      widgets: [
+        projectInfoWidget(),
+        githubActionsWidget({
+          layout: {
+            width: "small",
+            height: "small",
+          },
+          sites: [
+            {
+              siteName: "dumppi.fi",
+              githubUser: "Dumppiry",
+              githubRepo: "dumppi-website",
+              namespace: "dummpi-website",
+              eventType: "build-and-deploy-web",
+            },
+          ],
+        }),
+        catsWidget({ layout: { width: "medium" } }),
+      ],
+    }),
     deskTool({
       structure: deskStructure,
     }),
     visionTool(),
     colorInput(),
-    dashboardTool({
-      widgets: [
-        projectInfoWidget(),
-        catsWidget({ layout: { width: "medium" } }),
-      ],
-    }),
   ],
   schema: {
     types: schemas,
